b2d5235558e414afb3f311549dcb1d09dd60f349
liuwuyayui <72761058+liuwuyayui@users.noreply.github.com>
2021-05-14 21:30:03 -0700

    Add Basic Structures

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="footer">
+                (c)2021 Sate-Lite. All Rights Reserved. Website Made by Wuyang Liu
+            </footer>
+        )
+    }
+}
+
+export default Footer;
