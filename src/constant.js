fbf2e7664792d442a4b0e38fddf8c02400b46d69
liuwuyayui <72761058+liuwuyayui@users.noreply.github.com>
2021-05-14 23:35:03 -0700

    Implemented SatSetting.js SatelliteList.js

---
+++ src/constant.js
@@ -0,0 +1,8 @@
+export const SAT_BASE_URL = "https://www.n2yo.com/rest/v1/satellite";
+// export const SAT_BASE_URL = "/rest/v1/satellite";
+export const SAT_API_KEY = "3JZ6TV-L4TVV9-VXKYJN-4NWD";
+
+export const STARLINK_CATEGORY = "52";
+
+// export const NEARBY_SATELLITE = `${SAT_BASE_URL}/above`;
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
