3600fc06052c1299617f27e027f2022a08b7d032
wuyang-liu <liuwuyayui@gmail.com>
2022-03-05 23:15:24 -0800

    refactor: update PROXY using https

--- src/constant.js
+++ src/constant.js
@@ -6,5 +6,5 @@ export const SATELLITE_POSITION_URL = "positions";
 
 export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
 
-export const PERSONAL_PROXY = "http://personalwebhooks-env.eba-3bevfwtr.us-west-1.elasticbeanstalk.com/proxy";
+export const PERSONAL_PROXY = "https://webhooks.utils.click/proxy";
 // export const PERSONAL_PROXY = "http://localhost:8081/proxy";
